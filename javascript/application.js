48b1f0ea6f7ff7c21c3621a527657263c7e069e9
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2023-08-09 10:38:18 +0100

    vue demo

--- javascript/application.js
+++ javascript/application.js
@@ -4,6 +4,7 @@ createApp({
   data() {
     return {
       message: "Hello from Vue.JS",
+      userInput: "",
     };
   },
 }).mount("#app"); // The container where you want the Vue instance to be mounted.
